7140cfba6298927741b4b8cf77a8e95dba5c1c33
David Vurbs <david.v@xyndata.com>
2021-05-29 14:07:29 +0200

    json tags

--- models.go
+++ models.go
@@ -1,19 +1,19 @@
 package shieldwall
 
 type Identity struct {
-	Id        string
-	Namespace string
-	Roles     []string
+	Id        string   `json:"id"`
+	Namespace string   `json:"namespaces"`
+	Roles     []string `json:"roles"`
 }
 
 type Role struct {
-	Id         string
-	Namespaces []string
+	Id         string   `json:"id"`
+	Namespaces []string `json:"namespaces"`
 }
 
 type Policy struct {
-	Subject   string
-	Namespace string
-	Object    string
-	Effect    string
+	Subject   string `json:"id"`
+	Namespace string `json:"namespaces"`
+	Object    string `json:"object"`
+	Effect    string `json:"effect"`
 }
