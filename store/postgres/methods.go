fd1a6d5a9f215b395ad7b6e96924d9d10fbaeee7
David Vurbs <david.v@xyndata.com>
2021-05-31 01:43:23 +0200

    fix

--- store/postgres/methods.go
+++ store/postgres/methods.go
@@ -6,7 +6,7 @@ import (
 	"github.com/davevurby/shieldwall"
 )
 
-func (s *PgStore) CreatePolicy(policy shieldwall.Policy) error {
+func (s *PgStore) PutPolicy(policy shieldwall.Policy) error {
 	log.Printf("Upserting policy...\n")
 
 	_, err := s.db.Exec("insert into policy (subject, namespace, policy, effect) values ($1, $2, $3, $4) on conflict (subject, namespace, policy, effect) do nothing", policy.Subject, policy.Namespace, policy.Object, policy.Effect)
